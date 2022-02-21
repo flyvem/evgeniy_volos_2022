0757c43f68fffdf2263a2f1443a373209f786870
flyvem <flyvem@gmail.com>
2022-02-21 22:50:32 +0300

    start_21_02_2022

---
+++ evgeniy_volos_2022/evgeniy_volos_2022/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def about(request):
+    a = 5+6
+    return render(request,'about.html',{'gretting':a})
+
+def home(request):
+    return render(request, 'home.html')
