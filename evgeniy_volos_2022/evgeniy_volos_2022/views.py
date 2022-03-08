eb2a576a02fa3f4018ae75aad33e0966b5e49488
flyvem <flyvem@gmail.com>
2022-03-08 23:02:22 +0300

    08_03_2022

--- evgeniy_volos_2022/evgeniy_volos_2022/views.py
+++ evgeniy_volos_2022/evgeniy_volos_2022/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+# def about(request):
+#     a = 5+6
+#     return render(request,'about.html',{'gretting':a})
 
-def about(request):
-    a = 5+6
-    return render(request,'about.html',{'gretting':a})
+def reverse(request):
+    user_text = request.GET['username']
+    reverse = user_text[::-1]
+    return render(request,'reverse.html',{'word':reverse})
 
 def home(request):
     return render(request, 'home.html')
---
+++ evgeniy_volos_2022/project/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def project(request):
+    return render(request,'page_1.html')
